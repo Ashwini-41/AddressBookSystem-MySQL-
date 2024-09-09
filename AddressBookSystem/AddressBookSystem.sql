fccd58ed04b7799b3b58de474e982c5449a5bd61
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-10 01:07:28 +0530

    Added : Query to create table address book

--- AddressBookSystem/AddressBookSystem.sql
+++ AddressBookSystem/AddressBookSystem.sql
@@ -1,3 +1,19 @@
 create database address_book_service;
 use address_book_service;
 select database();
+create table AddressBook(
+id int auto_increment primary key,
+firstname varchar(50),
+lastname varchar(50),
+address varchar(100),
+city varchar(50),
+state varchar(50),
+zip INT,
+phoneNo varchar(12),
+email varchar(100)
+);
+
+describe AddressBook;
+
+
+
