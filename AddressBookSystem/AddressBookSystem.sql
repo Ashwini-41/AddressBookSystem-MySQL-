2abd8b3e8e5eec0b12fcea64f4692ff72b680e15
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-10 11:40:44 +0530

    Added : Query to insert contacts into addressbook table

--- AddressBookSystem/AddressBookSystem.sql
+++ AddressBookSystem/AddressBookSystem.sql
@@ -14,6 +14,14 @@ email varchar(100)
 );
 
 describe AddressBook;
+INSERT INTO AddressBook(firstname, lastname, address, city, state, zip, phoneNo, email) VALUES
+('Bill','John','201 Mega St','Pune','Maha',411041,9090898989,'john12@gmail.com'),
+('Rohn', 'Doe', '123 Main St', 'Solapur', 'Maha', 627040, '5551234234', 'john.doe@example.com'),
+('Riya', 'Sharma', '13 Street Road', 'Mumbai', 'Guj', 234346, '5678904321', 'roya.123@gmail.com'),
+('Shree','Varma','122 Flat No','Patana','Bihar','345678','9876543210','shreevarma@gmail.com'),
+('Siya','Sen','32 Main St','Lucknow','UP','234654','9876567897','siya321@gmail.com'),
+('Ashwini','Garad','Navi Peth','Pune','Maha','413041','8767898778','ashwini123@gmail.com');
 
+select * from AddressBook;
 
 
